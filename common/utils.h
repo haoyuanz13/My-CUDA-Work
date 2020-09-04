9e67a898d2f2c8abf810504bd52da5719c07ce4b
zhanghaoyuan <zhanghaoyuan@megvii.com>
2020-09-04 17:10:49 +0800

    refine utils source codes

---
+++ common/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+// check the cv::Mat type
+std::string type2str(int type);
+
+
+#endif // UTILS_H_
--- src/main_gray_normalize.cpp
+++ src/main_gray_normalize.cpp
@@ -1,40 +1,15 @@
 #include <iostream>
 #include <chrono>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 #include <assert.h>
+
+#include "utils.h"
 #include "kernel.h"
 
 #define MEAN_VAL 0.5
 #define STD_VAL 0.5
 
 
-/*
-  check the Mat type
-*/
-std::string type2str(int type) {
-  std::string r;
-
-  uchar depth = type & CV_MAT_DEPTH_MASK;
-  uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-  switch ( depth ) {
-    case CV_8U:  r = "8U"; break;
-    case CV_8S:  r = "8S"; break;
-    case CV_16U: r = "16U"; break;
-    case CV_16S: r = "16S"; break;
-    case CV_32S: r = "32S"; break;
-    case CV_32F: r = "32F"; break;
-    case CV_64F: r = "64F"; break;
-    default:     r = "User"; break;
-  }
-
-  r += "C";
-  r += (chans + '0');
-  return r;
-}
 
 
 /*
